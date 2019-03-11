ad5d032b88379b43c70a76e9c289026b842f555f
Ravid Cohen <ravidd9@gmail.com>
2019-03-11 15:19:02 +0200

    week 1 repo

---
+++ 11.3/js/main.js
@@ -0,0 +1,26 @@
+// const maxCapacity =14
+// let surfTime= true
+// let bestStudent
+// const purposeInLife ----> error
+// console.log(maxCapacity)
+// console.log(surfTime)
+// console.log(bestStudent)
+
+
+// let str= "r"
+// let result = str + " is the best thing ever"
+// console.log(result)
+
+
+// let password = "1234"
+// let confirmPassword = "1234"
+// if(password == confirmPassword){
+//   console.log("right")
+// }
+
+
+// console.log(423*12)
+// console.log(802/2)
+// console.log((5+6)*3)
+
+
---
+++ 11.3/js/main2.js
@@ -0,0 +1,68 @@
+// let numChildren = prompt ("enter number of children")
+// let isCareful = false
+// if(!isCareful){
+//   numChildren++
+// }
+// console.log(numChildren)
+
+
+// let silverwareCount = 3
+// if (silverwareCount % 2 !=0){
+//   console.log("there is something missing")
+// }
+
+
+// let performance = "stellar"
+// let salary = 10000
+// const goodBonus = 1000
+// const stellarBonus = 3000
+// if (performance == "stellar"){
+//   salary += stellarBonus
+// }
+// else{
+//   salary += goodBonus
+// }
+
+//4
+// const isVIP = false
+// let cash = 500
+// if(isVIP || cash > 300){
+//   console.log("welcome to the club")
+// }
+// else{
+//   consloe.log("stay out")
+// }
+
+// 6
+// const gender = "female"
+// let profession = "business"
+// if (gender == "male"){
+//   profession += "man"
+// }
+// else if(gender == "female"){
+//   profession += "woman"
+// }
+// else{
+//   profession += "person"
+// }
+// console.log(profession)
+
+//7
+// let boughtTesla = true
+// const yearOfTeslaPurchase = 2014
+// let isUSCitizen = true
+// let currentYear = 2018
+// if(boughtTesla && isUSCitizen){
+//   if(yearOfTeslaPurchase + 3 == currentYear){
+//     console.log("would you like to upgrade?")
+//   }
+//   else{
+//     console.log("are you satisfied with the car?")
+//   }
+// }
+// else if (boughtTesla && !isUSCitizen){
+//   console.log("would you like to move to the US?")
+// }
+// else if(!boughtTesla){
+//   console.log("would you like to buy tesla?")
+// }
